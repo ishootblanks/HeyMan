674f4a130a9e10e2cd189541b61c199d851efb1c
Damien <damienderail@yahoo.com>
2018-11-08 09:36:09 +0100

    track both point origin and destination

--- cheappi/src/app/dashboard/dashboard.component.ts
+++ cheappi/src/app/dashboard/dashboard.component.ts
@@ -13,27 +13,46 @@ import { MapComponent } from '../map/map.component';
 export class DashboardComponent implements OnInit {
 
   // input event
-  values = '';
+  marker1 = '';
+   marker2 = '';
 
-  onEnter(event: any) {
-    this.values += event.target.value + ' | ';
-    this.getLocation();
-  }
-  location: Location[];
+   onEnter(event: any) {
+     this.marker1 += event.target.value + ' | ';
+     this.getLocation();
+   }
+
+   onEnter2(event: any) {
+     this.marker2 += event.target.value + ' | ';
+     this.getDestination();
+   }
+
+   origin: Location[];
+   destination: Location[];
 
 
-  constructor(private apiClientService: ApiClientServiceService) { }
+   constructor(private apiClientService: ApiClientServiceService) { }
 
-    //transfer the input into a function
-    getLocation() {
-      this.apiClientService.getLocation(this.values).subscribe(response => {
-        this.location = response.results;
-      });
-    }
+     //transfer the input into a function
+     getLocation() {
+       this.apiClientService.getLocation(this.marker1).subscribe(response => {
+         this.origin = response.results;
+         console.log(this.origin);
+       });
+     }
 
-    ngOnInit() {
+     //transfer the input into a function
+     getDestination() {
+       this.apiClientService.getLocation(this.marker2).subscribe(response => {
+         this.destination = response.results;
+         console.log(this.destination);
+       });
+     }
 
-    }
+
+     ngOnInit() {
+
+     }
+
 
 
 }
