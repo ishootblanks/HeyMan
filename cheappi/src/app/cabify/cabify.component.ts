caa8dd093f6d32b2484851cd73fea08c5b76d870
Damien <damienderail@yahoo.com>
2018-11-08 22:43:04 +0100

    taxi service no console

--- cheappi/src/app/cabify/cabify.component.ts
+++ cheappi/src/app/cabify/cabify.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { TaxiService } from '../taxi.service';
+import { ApiClientServiceService } from '../api-client-service.service';
 
 
 @Component({
@@ -9,18 +10,29 @@ import { TaxiService } from '../taxi.service';
 })
 export class CabifyComponent implements OnInit {
 
-  fare: [];
+  private _fare = '';
 
-  constructor(private taxiService: TaxiService) { }
-
-  getFare() {
-    this.taxiService.getFare().subscribe(response => {
-      console.log(response.results);
-      this.fare = response.results;
-      console.log(this.fare);
-
-    });
+  @Input()
+  set fare(fare: string) {
+    console.log(fare);
+    this._fare = (fare && fare.trim()) || '<no name set>';
   }
+
+  get fare(): string { return this._fare; }
+
+  constructor(
+    private taxiService: TaxiService,
+    private apiClientService: ApiClientServiceService
+  ) { }
+
+  // getFare() {
+  //   this.taxiService.getFare().subscribe(response => {
+  //     console.log(response.results);
+  //     this.fare = response.results;
+  //     console.log(this.fare);
+  //
+  //   });
+  // }
 
   ngOnInit() {
   }
--- server/controllers/fare.js
+++ server/controllers/fare.js
@@ -4,9 +4,3 @@ exports.getFare = async (ctx, next) => {
   ctx.body = await fare.getEstimate();
   ctx.status = 200;
 }
-
-// exports.postFare = async (ctx, next) => {
-//   const newFare = await fare.postEstimate(ctx.request.body);
-//   ctx.send(newFare);
-//   ctx.status = 201;
-// }
