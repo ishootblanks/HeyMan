c5da1fad19890393213c4360d85405c597ea5df1
Damien <damienderail@yahoo.com>
2018-11-10 08:41:25 +0100

    move images to assets folder

--- cheappi/src/app/app.component.ts
+++ cheappi/src/app/app.component.ts
@@ -8,8 +8,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rapido';
-  myImage : string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZOBbyZXF9_Gb5CbUeb_WhlBhXLFqy3eJLqNmZGaLBL2HMpX_C9Q";
+  title = 'Hey.MaN!';
+  myImage : string = "assets/logo.png";
 
   constructor(private router: Router) {}
 
