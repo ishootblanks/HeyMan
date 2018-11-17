b3b514da81c6bdf371e0e3a37554ae4209bb062c
Berta Cume <bertacume@gmail.com>
2018-11-17 19:32:13 +0100

    unit testing dashboard and api-client-service

--- cheappi/src/app/api-client-service.service.spec.ts
+++ cheappi/src/app/api-client-service.service.spec.ts
@@ -1,16 +1,19 @@
-import { TestBed } from '@angular/core/testing';
-
 import { ApiClientServiceService } from './api-client-service.service';
 import { HttpClient } from '@angular/common/http';
 
+
 describe('ApiClientServiceService', () => {
-  let comp: ApiClientServiceService
+  let httpClient: HttpClient;
+  let service: ApiClientServiceService;
+
   beforeEach(() => {
-    comp = new ApiClientServiceService(new HttpClient())
+    const httpClient = new HttpClient(undefined);
+    service = new ApiClientServiceService(httpClient)
   });
 
   it('should be created', () => {
-    const service: ApiClientServiceService = comp;
-    expect(true).toBeTruthy();
+    expect(service).toBeTruthy();
   });
 });
+
+
--- cheappi/src/app/dashboard/dashboard.component.spec.ts
+++ cheappi/src/app/dashboard/dashboard.component.spec.ts
@@ -1,25 +1,35 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DashboardComponent } from './dashboard.component';
+import { ApiClientServiceService } from '../api-client-service.service';
+import { of } from 'rxjs';
+
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
+  let service: ApiClientServiceService;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => { 
+    service = new ApiClientServiceService(undefined);
+    component = new DashboardComponent(service);
   });
 
-  it('should create', () => {
+  it('should created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('#getOrigin should get Origin coordinates', () => {
+    const data = {results: [{geometry: {location: {
+      lat: 41.4039286,
+      lng: 2.181874
+    }}}]};
+
+    service.getLocation = () => of(data); 
+    
+    component.getOrigin();
+    component.ngOnInit();
+
+    console.log(component);
+    
+    expect(component.origin.lat).toBe(41.4039286);
+    expect(component.origin.lng).toBe(2.181874);
+  })
 });
