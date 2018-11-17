65003df9ba920af24c852a58e358cbda35a9249b
Berta Cume <bertacume@gmail.com>
2018-11-17 21:39:52 +0100

    dashboard unit testing

--- cheappi/src/app/dashboard/dashboard.component.spec.ts
+++ cheappi/src/app/dashboard/dashboard.component.spec.ts
@@ -1,25 +1,34 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DashboardComponent } from './dashboard.component';
+import { ApiClientServiceService } from '../api-client-service.service';
+import { of } from 'rxjs';
+
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
+  let service: ApiClientServiceService;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => { 
+    service = new ApiClientServiceService(undefined);
+    component = new DashboardComponent(service);
   });
 
-  it('should create', () => {
+  it('should created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('#getCoordinates should assign coordinates to #origin and #destination', () => {
+    const data = {results: [{geometry: {location: {
+      lat: 41.4039286,
+      lng: 2.181874
+    }}}]};
+
+    service.getLocation = () => of(data); 
+    
+    component.onClickMe();
+
+    expect(component.origin.lat).toBe(41.4039286);
+    expect(component.origin.lng).toBe(2.181874);
+    expect(component.destination.lat).toBe(41.4039286);
+    expect(component.destination.lng).toBe(2.181874);
+  })
 });
